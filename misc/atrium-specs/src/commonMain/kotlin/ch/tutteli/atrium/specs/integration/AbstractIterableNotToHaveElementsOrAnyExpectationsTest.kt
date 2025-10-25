8a819cf9769575cb20d430bbf87cf55f86fc05b2
Tomer Ben-Rachel <tomerpacific@gmail.com>
2025-10-25 19:47:05 +0300

    issue-2016 replacing subjectless tests and assertioncreator tests

--- misc/atrium-specs/src/commonMain/kotlin/ch/tutteli/atrium/specs/integration/AbstractIterableNotToHaveElementsOrAnyExpectationsTest.kt
+++ misc/atrium-specs/src/commonMain/kotlin/ch/tutteli/atrium/specs/integration/AbstractIterableNotToHaveElementsOrAnyExpectationsTest.kt
@@ -4,39 +4,57 @@ import ch.tutteli.atrium.api.fluent.en_GB.*
 import ch.tutteli.atrium.api.verbs.internal.expect
 import ch.tutteli.atrium.creating.Expect
 import ch.tutteli.atrium.specs.*
+import ch.tutteli.atrium.specs.integration.IterableToContainSpecBase.Companion.oneToSeven
+import ch.tutteli.atrium.specs.integration.utils.ExpectationCreatorTestData
+import ch.tutteli.atrium.specs.integration.utils.SubjectLessTestData
+import ch.tutteli.atrium.testfactories.TestFactory
 import ch.tutteli.atrium.translations.DescriptionIterableLikeExpectation
 
+@Suppress("FunctionName")
 abstract class AbstractIterableNotToHaveElementsOrAnyExpectationsTest(
-    notToHaveElementsOrAny: Fun1<Iterable<Double>, Expect<Double>.() -> Unit>,
-    notToHaveElementsOrAnyNullable: Fun1<Iterable<Double?>, (Expect<Double>.() -> Unit)?>,
-    describePrefix: String = "[Atrium] "
-) : IterableToContainEntriesSpecBase({
+    private val notToHaveElementsOrAnySpec: Fun1<Iterable<Double>, Expect<Double>.() -> Unit>,
+    private val notToHaveElementsOrAnyNullableSpec: Fun1<Iterable<Double?>, (Expect<Double>.() -> Unit)?>
+) : ExpectationFunctionBaseTest() {
 
-    include(object : SubjectLessSpec<Iterable<Double>>(describePrefix,
-        notToHaveElementsOrAny.forSubjectLessTest { toEqual(2.5) }
-    ) {})
-    include(object : SubjectLessSpec<Iterable<Double?>>(
-        describePrefix,
-        notToHaveElementsOrAnyNullable.forSubjectLessTest(null)
-    ) {})
+    @TestFactory
+    fun subjectLessTest() = subjectLessTestFactory(
+        SubjectLessTestData(
+            notToHaveElementsOrAnySpec.forSubjectLessTest { toEqual(2.5) }
+        ),
+        SubjectLessTestData(
+            notToHaveElementsOrAnyNullableSpec.forSubjectLessTest(null)
+        )
+    )
 
-    include(object : AssertionCreatorSpec<Iterable<Double>>(
-        describePrefix, oneToSeven().toList().asIterable(),
-        notToHaveElementsOrAny.forExpectationCreatorTest("$toBeGreaterThanDescr: 1.1") { toBeGreaterThan(1.1) }
-    ) {})
-    include(object : AssertionCreatorSpec<Iterable<Double?>>(
-        "$describePrefix[nullable Element] ", oneToSeven().toList().asIterable(),
-        notToHaveElementsOrAnyNullable.forExpectationCreatorTest("$toBeGreaterThanDescr: 1.1") { toBeGreaterThan(1.1) }
-    ) {})
+    @TestFactory
+    fun expectationCreatorTest() = expectationCreatorTestFactory(
+        ExpectationCreatorTestData(
+            oneToSeven().toList().asIterable(),
+            notToHaveElementsOrAnySpec.forExpectationCreatorTest("$toBeGreaterThanDescr: 1.1") { toBeGreaterThan(1.1) }
+        ),
+        ExpectationCreatorTestData(
+            oneToSeven().toList().asIterable(),
+            notToHaveElementsOrAnyNullableSpec.forExpectationCreatorTest("$toBeGreaterThanDescr: 1.1") {
+                toBeGreaterThan(
+                    1.1
+                )
+            }
+        )
+    )
 
     val notToHaveElementsOrAnyDescr = DescriptionIterableLikeExpectation.NOT_TO_HAVE_ELEMENTS_OR_ANY.getDefault()
     val noSuchElementDescr = DescriptionIterableLikeExpectation.NEITHER_EMPTY_NOR_ELEMENT_FOUND.getDefault()
 
+
+    @TestFactory
+    fun 
     nonNullableCases(
-        describePrefix,
-        notToHaveElementsOrAny,
-        notToHaveElementsOrAnyNullable
-    ) { notToHaveElementsOrAnyFun ->
+    describePrefix,
+    notToHaveElementsOrAny,
+    notToHaveElementsOrAnyNullable
+    )
+    {
+        notToHaveElementsOrAnyFun ->
 
         context("empty collection") {
             it("does not throw") {
@@ -68,10 +86,11 @@ abstract class AbstractIterableNotToHaveElementsOrAnyExpectationsTest(
         }
     }
 
-    nullableCases(describePrefix) {
+    nullableCases(describePrefix)
+    {
 
-        describeFun(notToHaveElementsOrAnyNullable) {
-            val notToHaveElementsOrAnyFun = notToHaveElementsOrAnyNullable.lambda
+        describeFun(notToHaveElementsOrAnyNullableSpec) {
+            val notToHaveElementsOrAnyFun = notToHaveElementsOrAnyNullableSpec.lambda
 
             context("iterable ${oneToSevenNullable().toList()}") {
                 context("happy cases (do not throw)") {
